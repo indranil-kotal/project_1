d78d0ea8e2418ac0bbc9ca4ca2117a30ec549c18
indranil-kotal <124678609+indranil-kotal@users.noreply.github.com>
2025-03-22 23:19:49 +0530

    Delete 20-03-35 html css (responsive) directory

--- 20-03-35 html css (responsive)/js/app.js
+++
@@ -1,5 +0,0 @@
-const menuBtn = document.querySelector(".menu-btn");
-const menuList = document.querySelector(".nav-links");
-menuBtn.addEventListener("click", () => {
-  menuList.classList.toggle("active");
-});
